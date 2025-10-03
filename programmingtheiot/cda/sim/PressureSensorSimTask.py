4085030dac899c46fa3819a55ac2c4a5ed7d731a
nishchint98 <42786755+nishchint98@users.noreply.github.com>
2025-10-02 19:29:59 -0500

    PIOT-CDA-03-003: Create all sensor simulator modules (derived from BaseSensorSimTask)

--- programmingtheiot/cda/sim/PressureSensorSimTask.py
+++ programmingtheiot/cda/sim/PressureSensorSimTask.py
@@ -12,6 +12,8 @@
 
 import logging
 
+import programmingtheiot.common.ConfigConst as ConfigConst
+
 from programmingtheiot.cda.sim.BaseSensorSimTask import BaseSensorSimTask
 from programmingtheiot.cda.sim.SensorDataGenerator import SensorDataGenerator
 
@@ -23,6 +25,12 @@ class PressureSensorSimTask(BaseSensorSimTask):
 	
 	"""
 
-	def __init__(self):
-		pass
+	def __init__(self, dataSet = None):
+		super( \
+			PressureSensorSimTask, self).__init__( \
+				name = ConfigConst.PRESSURE_SENSOR_NAME, \
+				typeID = ConfigConst.PRESSURE_SENSOR_TYPE, \
+				dataSet = dataSet, \
+				minVal = SensorDataGenerator.LOW_NORMAL_ENV_PRESSURE, \
+				maxVal = SensorDataGenerator.HI_NORMAL_ENV_PRESSURE)
 	
