d4d1573d47cf3ea859e77a5d8c058ee0b986fa4f
nishchint98 <42786755+nishchint98@users.noreply.github.com>
2025-10-26 01:07:07 -0500

    PIOT-CDA-08-002: Create resource handlers for data requests from GDA to CDA

--- programmingtheiot/cda/connection/handlers/GetSystemPerformanceResourceHandler.py
+++ programmingtheiot/cda/connection/handlers/GetSystemPerformanceResourceHandler.py
@@ -12,15 +12,21 @@
 
 import logging
 
+from coapthon import defines
+from coapthon.resources.resource import Resource
+
 import programmingtheiot.common.ConfigConst as ConfigConst
 
 from programmingtheiot.common.ConfigUtil import ConfigUtil
 from programmingtheiot.common.ITelemetryDataListener import ITelemetryDataListener
+from programmingtheiot.common.IDataMessageListener import IDataMessageListener
+from programmingtheiot.common.ISystemPerformanceDataListener import ISystemPerformanceDataListener 
 
 from programmingtheiot.data.DataUtil import DataUtil
+
 from programmingtheiot.data.SystemPerformanceData import SystemPerformanceData
 
-class GetSystemPerformanceResourceHandler(ITelemetryDataListener):
+class GetSystemPerformanceResourceHandler(Resource, ISystemPerformanceDataListener):
 	"""
 	Observable resource that will collect system performance data based on the
 	given name from the data message listener implementation.
@@ -30,9 +36,48 @@ class GetSystemPerformanceResourceHandler(ITelemetryDataListener):
 	
 	"""
 
-	def __init__(self):
-		pass
+	def __init__(self, name: str = ConfigConst.SYSTEM_PERF_MSG, coap_server = None, dataMsgListener: IDataMessageListener = None):
+		super(GetSystemPerformanceResourceHandler, self).__init__( \
+			name, coap_server, visible = True, observable = True, allow_children = True)
+
+		self.pollCycles = \
+			ConfigUtil().getInteger( \
+				section = ConfigConst.CONSTRAINED_DEVICE, \
+				key = ConfigConst.POLL_CYCLES_KEY, \
+				defaultVal = ConfigConst.DEFAULT_POLL_CYCLES)
 		
+		self.dataUtil = DataUtil()
+		self.sysPerfData = SystemPerformanceData()
+		
+		self.dataMsgListener = dataMsgListener
+		
+		# reserved for lab module 10
+		if self.dataMsgListener:
+			self.dataMsgListener.setSystemPerformanceDataListener(self)
+		
+		# for testing
+		self.payload = "GetSysPerfData"
+		
+	def render_GET_advanced(self, request, response):
+		if request:
+			response.code = defines.Codes.CONTENT.number
+			
+			if not self.sysPerfData:
+				response.code = defines.Codes.EMPTY.number
+				self.sysPerfData = SystemPerformanceData()
+				
+			jsonData = DataUtil().systemPerformanceDataToJson(self.sysPerfData)
+			
+			logging.info("Latest SystemPerformanceData JSON: " + jsonData)
+			
+			response.payload = (defines.Content_types["application/json"], jsonData)
+			response.max_age = self.pollCycles
+	
+			# 'changed' will be discussed in a later exercise
+			self.changed = False
+				
+		return self, response
+	
 	def onSystemPerformanceDataUpdate(self, data: SystemPerformanceData) -> bool:
-		pass
+		self.sysPerfData = data
 	
--- programmingtheiot/cda/connection/handlers/GetTelemetryResourceHandler.py
+++ programmingtheiot/cda/connection/handlers/GetTelemetryResourceHandler.py
@@ -12,27 +12,70 @@
 
 import logging
 
+from coapthon import defines
+from coapthon.resources.resource import Resource
+
 import programmingtheiot.common.ConfigConst as ConfigConst
 
 from programmingtheiot.common.ConfigUtil import ConfigUtil
+from programmingtheiot.common.IDataMessageListener import IDataMessageListener
 from programmingtheiot.common.ITelemetryDataListener import ITelemetryDataListener
 
 from programmingtheiot.data.DataUtil import DataUtil
 from programmingtheiot.data.SensorData import SensorData
 
-class GetTelemetryResourceHandler(ITelemetryDataListener):
+class GetTelemetryResourceHandler(Resource, ITelemetryDataListener):
 	"""
-	Observable resource that will collect telemetry based on the given
-	name from the data message listener implementation.
+	Observable resource that will collect sensor data based on the
+	given name from the data message listener implementation.
 	
 	NOTE: Your implementation will likely need to extend from the selected
 	CoAP library's observable resource base class.
 	
 	"""
 
-	def __init__(self):
-		pass
+	def __init__(self, name: str = ConfigConst.SENSOR_MSG, coap_server = None, dataMsgListener: IDataMessageListener = None):
+		super(GetTelemetryResourceHandler, self).__init__( \
+			name, coap_server, visible = True, observable = True, allow_children = True)
+
+		self.pollCycles = \
+			ConfigUtil().getInteger( \
+				section = ConfigConst.CONSTRAINED_DEVICE, \
+				key = ConfigConst.POLL_CYCLES_KEY, \
+				defaultVal = ConfigConst.DEFAULT_POLL_CYCLES)
 		
-	def onSensorDataUpdate(self, data: SensorData = None) -> bool:
-		pass
+		self.dataUtil = DataUtil()
+		self.sensorData = SensorData()
+		
+		self.dataMsgListener = dataMsgListener
+		
+		# reserved for lab module 10
+		if self.dataMsgListener:
+			self.dataMsgListener.setTelemetryDataListener(self)
+		
+		# for testing
+		self.payload = "GetSensorData"
+		
+	def render_GET_advanced(self, request, response):
+		if request:
+			response.code = defines.Codes.CONTENT.number
+			
+			if not self.sensorData:
+				response.code = defines.Codes.EMPTY.number
+				self.sensorData = SensorData()
+				
+			jsonData = DataUtil().sensorDataToJson(self.sensorData)
+			
+			logging.info("Latest SensorData JSON: " + jsonData)
+			
+			response.payload = (defines.Content_types["application/json"], jsonData)
+			response.max_age = self.pollCycles
+	
+			# 'changed' will be discussed in a later exercise
+			self.changed = False
+				
+		return self, response
+	
+	def onSensorDataUpdate(self, data: SensorData) -> bool:
+		self.sensorData = data
 	
